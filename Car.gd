242092ec026b61fb588af48441a485a3c758ef09
derhannesb <johannes.bade@gmail.com>
2016-03-10 20:12:24 +0100

    add cytrill compability

--- Car.gd
+++ Car.gd
@@ -1,34 +1,33 @@
 
 extends RigidBody2D
 
-var nose_norm = vec2(0,0)
+export var player_number = 0
+var joy_tresh = 0.2
+
+var nose_norm = Vector2(0,0)
 var accel = 60.0
 var sun = null
 var shadow = null
 var sound_motor = 0
 var sound_skid = -1
+
 func _ready():
-	
 	add_to_group("Cars")
 	sun = get_node("/root/World/Sun")
 	shadow = get_node("Shadow")
 	set_fixed_process(true)
 	sound_motor = get_node("SamplePlayer2D").play("engine3")
 	
-	
-	
 func _fixed_process(delta):
-	
-	
 	#print(str(get_linear_velocity().length()))
-	nose_norm = vec2(get_node("Nose").get_global_pos().x - get_pos().x, get_node("Nose").get_global_pos().y - get_pos().y).normalized()
-	if (Input.is_action_pressed(get_name() + "_Up")):
+	nose_norm = Vector2(get_node("Nose").get_global_pos().x - get_pos().x, get_node("Nose").get_global_pos().y - get_pos().y).normalized()
+	if Input.is_joy_button_pressed(player_number,  1):
 		accel = abs(accel)
-		apply_impulse(vec2(0,0),nose_norm*accel)
-	if (Input.is_action_pressed(get_name() + "_Down")):
+		apply_impulse(Vector2(0,0),nose_norm*accel)
+	if Input.is_joy_button_pressed(player_number,  2):
 		accel = -abs(accel)
-		apply_impulse(vec2(0,0),nose_norm*accel)
-	print(get_linear_velocity().length())
+		apply_impulse(Vector2(0,0),nose_norm*accel)
+	
 	get_node("SamplePlayer2D").set_param(SamplePlayer2D.PARAM_PITCH_SCALE, .75 + get_linear_velocity().length()/170)
 	
 	if (abs(get_linear_velocity().angle_to(nose_norm)) > 0.5 && get_linear_velocity().length() > 150):
@@ -41,19 +40,16 @@ func _fixed_process(delta):
 		elif (!get_node("SampleSkid").is_voice_active(sound_skid)):
 			#get_node("SampleSkid").stop_voice(sound_skid)
 			sound_skid = get_node("SampleSkid").play("skid")
-			
 		
-		
-	if (Input.is_action_pressed(get_name() + "_Left")):
+	if Input.get_joy_axis(player_number,  0) < -joy_tresh:
 			set_rot(get_rot()+ min (.015*get_linear_velocity().length()*delta, 0.05) * (accel/abs(accel)))
 			#set_rot(get_rot()+1*delta)
 			
-	if (Input.is_action_pressed(get_name() + "_Right")):
+	if Input.get_joy_axis(player_number, 0) > joy_tresh:
 			set_rot(get_rot()- min (.015*get_linear_velocity().length()*delta, 0.05)* (accel/abs(accel))) 
 			#set_rot(get_rot()-1*delta)
-			
 	
-	shadow.set_offset(vec2(15,25).rotated(-get_rot()))
+	shadow.set_offset(Vector2(15,25).rotated(-get_rot()))
 	
 func _integrate_forces(state):
 	if (state.get_contact_count() > 0):
--- main.gd
+++ main.gd
@@ -2,6 +2,12 @@
 extends Node2D
 
 var time_elapsed = 0
+var spawn_time = 100
+
+const colarray = [Color(0, 0, 1), Color(0, 1, 0), Color(0, 1, 1),
+	Color(1, 0, 0), Color(1, 0, 1), Color(1, 1, 0), Color(1, 1, 1), Color(0, 0, 0)]
+
+const player_preload = preload("player.scn")
 
 func _ready():
 	set_fixed_process(true)
@@ -10,9 +16,22 @@ func _fixed_process(delta):
 	time_elapsed += delta
 	get_node("Skidmarks").set_texture(get_node("Viewport").get_render_target_texture())
 	
-
-
-
+	if time_elapsed < spawn_time:
+		for i in range(128):
+			if Input.is_joy_button_pressed(i, 0):
+				var exists = false
+				for child in get_node("/root/World").get_children():
+					if "Cars" in child.get_groups():
+						if child.player_number == i:
+							exists = true
+				if !exists:
+					var new_player = player_preload.instance()
+					new_player.player_number = i
+					new_player.set_pos(get_node("Goal").get_pos())
+					new_player.get_node("Sprite").set_modulate(colarray[i%8])
+					leds.set_led(i, 0, colarray[i%8].r*255, colarray[i%8].g*255, colarray[i%8].b*255, 7)
+					leds.set_led(i, 1, colarray[i%8].r*255, colarray[i%8].g*255, colarray[i%8].b*255, 7)
+					get_node("/root/World").add_child(new_player)
 
 func _on_raiseZIndex_body_enter( body ):
 	pass # replace with function body
